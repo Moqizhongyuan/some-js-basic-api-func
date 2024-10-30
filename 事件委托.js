63095c0e88abf8d35deca0776b911ed240d23824
Moqizhongyuan <2356373295@qq.com>
2024-10-30 09:50:45 +0800

    补充

--- 事件委托.js
+++ 事件委托.js
@@ -3,3 +3,13 @@ document.querySelector("ul").onclick = (event) => {
 };
 
 // 这里是在事件冒泡的阶段触发的事件
+
+// 获取节点内容
+const element = document.getElementById("myElement");
+const htmlContent = element.innerHTML; // 获取所有标签和文本
+const textContent1 = element.innerText; // 获取所有文本
+const textContent2 = element.textContent; // 获取所有文本 + 隐藏文本
+const srcValue = element.getAttribute("src"); // 获取属性值
+
+const inputElement = document.getElementById("myInput");
+const userInput = inputElement.value; // 获取表单的input值
