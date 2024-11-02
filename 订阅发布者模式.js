6fa50e26ce54cbedbf6d9e2a1099fc763b8cf1bd
Moqizhongyuan <2356373295@qq.com>
2024-11-02 11:20:46 +0800

    listener

---
+++ 订阅发布者模式.js
@@ -0,0 +1,26 @@
+class EventBus {
+  constructor() {
+    this.listeners = {};
+  }
+
+  // 添加订阅者
+  on(event, listener) {
+    if (!this.listeners[event]) {
+      this.listeners[event] = [];
+    }
+    this.listeners[event].push(listener);
+  }
+
+  // 发布事件
+  emit(event, data) {
+    if (this.listeners[event]) {
+      this.listeners[event].forEach((listener) => listener(data));
+    }
+  }
+
+  // 移除订阅者
+  off(event, listener) {
+    if (!this.listeners[event]) return;
+    this.listeners[event] = this.listeners[event].filter((l) => l !== listener);
+  }
+}
